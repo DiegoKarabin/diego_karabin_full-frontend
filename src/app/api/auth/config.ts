3488db9f5fa0db454e36f202eb4b1f24c94cc571
Diego Karabin <diegokarabin@gmail.com>
2025-04-02 21:15:34 -0400

    fix: fixing vercel deploy errors

---
+++ src/app/api/auth/config.ts
@@ -0,0 +1,28 @@
+import { AuthOptions } from 'next-auth';
+import SpotifyProvider from 'next-auth/providers/spotify';
+
+export const authOptions: AuthOptions = {
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: 'user-library-read user-library-modify',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+};
